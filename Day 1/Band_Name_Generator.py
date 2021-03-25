ca7655292e0416f0e56c4be138525177a5b907a2
Dan Murray <54911552+ha3ks@users.noreply.github.com>
2021-03-25 11:48:21 +0000

    Day 1
    Pushing all my code

---
+++ Day 1/Band_Name_Generator.py
@@ -0,0 +1,15 @@
+#Band Name Generator
+
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator, rock on!")
+#2. Ask the user for the city that they grew up in.
+city = input("What city did you grow up in? ")
+#3. Ask the user for the name of a pet.
+pet  = input("What was your pets name? ")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your Band Name could be something cool like " + city + " " + pet 
++ ", Hell yeah! Now go rock someones face off!")
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
+
+#I'm not using Repl like suggested on the course, I started in Hard Mode with Visual Studio Code on my machine and the many issues getting git to work and also python, you can install it from the web fair enough but try running it in VS Code and suddenly its all like 'whoa dude I don't have Python installed, go get it from the Microsoft store' and im like WTF dude its installed... fine ill uninstall it and grab 3.9 from the windows store, bitch... oh now it suddenly works seemlessly... ah well can't complain too much its working and that the most important part. Anyhoos if you made it this far congratulations and ill see you tomorrow for Day 2 of the 100 days of code.
---
+++ Day 1/main.py
@@ -0,0 +1,77 @@
+# Day 1
+
+#initial print statements x3
+print("Day 1 - Python Print Function")
+print("The Function is declared like this")
+print("print('what to print')")
+
+
+#single line print statement to print on multiple lines using '\n'
+print("Day 1 - Python Print Function\nThe Function is declared like this\nprint('what to print')")
+
+
+#playing with spaces
+print("Hello" + " Angela")
+print("Hello" + " " + "Angela")
+
+
+#Debugging Exercise
+#print(Day 1 - String Manipulation")
+#print("String Concatenation is done with "+" sign.")
+# print('e.g. print ("Hello " + "world")')
+#print(("New lines can be created with a backslash and n.")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with "+" sign.')
+print('e.g. print ("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+#the input funtion
+input("What is your name?")
+
+
+#taking that input and displaying it back to the user
+print("Hello " + input("What is your name?"))
+
+input("What is your name? ")
+
+
+#display the char length using len
+print(len(input("What is your name? ")))
+
+
+#assigning variables
+name = input("What is your name? ")
+print("Hello " + name)
+
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+
+#Variables exercise
+##########
+###Dont change
+a = input("a:")
+b = input("b:")
+###
+###write code here
+c=a
+a=b
+b=c 
+###Dont change
+print("a = " + a)
+print("b = " + b)
+##########
+
+#Naming Variables
+n = input("What is your name?")
+l = len(n)
+print(l)
