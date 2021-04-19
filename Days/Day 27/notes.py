864a0f5bef8fb2aed6ffff53165b819ea8916885
ha3ks <54911552+ha3ks@users.noreply.github.com>
2021-04-20 00:12:19 +0100

    Day 27(?) may have a rest day today.

---
+++ Days/Day 27/notes.py
@@ -0,0 +1,4 @@
+#Day 27
+#Felt the real struggle bus yesterday, lets see how I get on with a fresh set of eyes. I know the course pledge says 100 days of code but it does not say consecutive so I feel I can get awat with having one day off as long as I justify and note it... becuase Github streak! like a kill streak in COD but instead of 'AGM online' its nerd cred!
+
+#
