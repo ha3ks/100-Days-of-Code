cc4f042858b7ea8126087fcd2e098eda793178ca
ha3ks <54911552+ha3ks@users.noreply.github.com>
2021-04-21 08:00:20 +0100

    Day 28 *cries* Ill actually do 27 and onward soon.

--- Days/Day 27/notes.py
+++ Days/Day 27/notes.py
@@ -1,4 +1,4 @@
 #Day 27
 #Felt the real struggle bus yesterday, lets see how I get on with a fresh set of eyes. I know the course pledge says 100 days of code but it does not say consecutive so I feel I can get awat with having one day off as long as I justify and note it... becuase Github streak! like a kill streak in COD but instead of 'AGM online' its nerd cred!
 
-#
+#Day 28 technically, Still feeling rough.. not a COVID Jab2 rough or anything, thats booked next month. Just life comes at you hard and fast sometimes.. more updates soon. may even consider taking up coding/hacking streaming to pass time.
